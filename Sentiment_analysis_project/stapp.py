8bd519a431fbd74cf42c456b5037a16915a68316
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-08-29 18:40:20 +0530

    app ui improvied

--- Sentiment_analysis_project/stapp.py
+++ Sentiment_analysis_project/stapp.py
@@ -13,6 +13,10 @@ st.title("😎 Movie Review Rating Predictor")
 review = st.text_area("Write your movie review below, and we'll predict the rating for you!")
 
 if st.button("Predict"):
+    st.write(f"review: {review}")
     result = predict({'review': review})
+    rating = result['rating']
+    ir = int(float(rating))
     st.write("### The predicted rating is" )
-    st.write(f"# {result['rating']}")
+    st.write(f"# {rating}")
+    st.write(f"## {ir*'⭐'} {(10-ir)*' __ '}")
